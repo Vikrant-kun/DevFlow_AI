e1fda1a324b926b38a77ed1436f2723e89d7faea
Vikrant-kun <vikrantvinchurkar12@gmail.com>
2026-03-11 00:37:09 +0530

    fix(api): correct github integration settings route

--- src/lib/apiRoutes.js
+++ src/lib/apiRoutes.js
@@ -2,7 +2,7 @@ export const API_ROUTES = {
     runs: "/runs/",
     workflows: "/workflows/",
     workflowRun: "/workflows/run/",
-    githubSettings: "/github/settings/",
+    githubSettings: "/github/integration-settings/",
     githubRepos: "/github/repos/",
     githubSelectRepo: "/github/select-repo/",
     githubSelectedRepo: "/github/selected-repo/",
